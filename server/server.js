ed423d1647290bf2beea23b6b5a656a5bf730520
Roger Le <rogermle@gmail.com>
2022-03-29 13:03:01 -0400

    Adding route to load React Frontend

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(PORT, ()=>{
     console.log("App is listening on: http://localhost:" + PORT)
